a55c54082a6bd6da949b4815f0e0befccd51f10b
Anton Suslennikov <Anton.Suslennikov@bsc-ideas.com>
2019-05-15 22:08:45 +0300

    Simple version of splash screen

--- app/src/main/java/ru/voxp/android/RootActivity.kt
+++ app/src/main/java/ru/voxp/android/RootActivity.kt
@@ -3,10 +3,22 @@ package ru.voxp.android
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+const val SPLASH_FRAGMENT_TAG: String = "SPLASH"
+
 class RootActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.root_activity)
+
+        var splashFragment = supportFragmentManager.findFragmentByTag(SPLASH_FRAGMENT_TAG)
+        if (splashFragment == null) {
+            splashFragment = SplashFragment()
+        }
+        if (!splashFragment.isVisible) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.root_activity_content_placeholder, splashFragment, SPLASH_FRAGMENT_TAG)
+                .commit()
+        }
     }
 }
---
+++ app/src/main/java/ru/voxp/android/SplashFragment.kt
@@ -0,0 +1,18 @@
+package ru.voxp.android
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.splash_fragment, container, false)
+    }
+}
