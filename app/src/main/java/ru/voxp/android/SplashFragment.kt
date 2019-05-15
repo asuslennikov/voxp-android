864ae75290f82c14bf3c66408ff14db0f6f78279
Anton Suslennikov <Anton.Suslennikov@bsc-ideas.com>
2019-05-16 00:26:38 +0300

    Splash screen logo animation

--- app/src/main/java/ru/voxp/android/SplashFragment.kt
+++ app/src/main/java/ru/voxp/android/SplashFragment.kt
@@ -1,9 +1,11 @@
 package ru.voxp.android
 
+import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 
 class SplashFragment : Fragment() {
@@ -12,7 +14,16 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.splash_fragment, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val ivLogo = view?.findViewById<ImageView>(R.id.splash_fragment_logo)
+        val logoDrawable = ivLogo?.drawable
+        if (logoDrawable is Animatable) {
+            logoDrawable.start()
+        }
+
     }
 }
