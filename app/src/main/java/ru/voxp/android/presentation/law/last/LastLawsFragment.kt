56341294ac8a4726e89a9962eee987cc8cac4a2e
Anton Suslennikov <Anton.Suslennikov@bsc-ideas.com>
2019-06-20 20:15:20 +0300

    Switch to AppBar (broken shared element transition)

--- app/src/main/java/ru/voxp/android/presentation/law/last/LastLawsFragment.kt
+++ app/src/main/java/ru/voxp/android/presentation/law/last/LastLawsFragment.kt
@@ -1,5 +1,7 @@
 package ru.voxp.android.presentation.law.last
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.transition.ChangeBounds
 import android.view.LayoutInflater
@@ -19,7 +21,7 @@ class LastLawsFragment : Fragment<LastLawsState, LastLawsViewModel, LastLawsFrag
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = super.onCreateView(inflater, container, savedInstanceState)
         sharedElementEnterTransition = ChangeBounds().apply {
-            duration = 250L
+            duration = getLong(R.integer.last_law_fragment_shared_element_transition_duration)
         }
         binding.lastLawsFragmentList.layoutManager = LinearLayoutManager(context)
         binding.lastLawsFragmentList.adapter = LawCardAdapter(viewModel.lawCardViewModelRegistry)
@@ -29,5 +31,31 @@ class LastLawsFragment : Fragment<LastLawsState, LastLawsViewModel, LastLawsFrag
     override fun render(screenState: LastLawsState) {
         super.render(screenState)
         (binding.lastLawsFragmentList.adapter as LawCardAdapter).setData(screenState.laws)
+        changeViewVisibility(screenState.lawsVisible, binding.lastLawsFragmentList)
+        changeViewVisibility(screenState.loaderVisible, binding.lastLawsFragmentLoaderContainer)
+        changeViewVisibility(screenState.noInternetVisible, binding.lastLawsFragmentNoInternetContainer)
+    }
+
+    private fun getLong(resId: Int): Long {
+        return context?.resources?.getInteger(resId)?.toLong() ?: 0L
+    }
+
+    private fun changeViewVisibility(targetVisible: Boolean, targetView: View) {
+        val targetVisibilityState = if (targetVisible) View.VISIBLE else View.GONE
+        val currentVisibilityState = targetView.visibility
+        if (targetVisibilityState != currentVisibilityState) {
+            targetView.animate()
+                .alpha(if (targetVisible) 0f else 1.0f)
+                .setDuration(getLong(R.integer.last_law_fragment_visibility_fade_duration))
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        if (isAdded) {
+                            targetView.visibility = targetVisibilityState
+                            targetView.alpha = if (targetVisible) 1.0f else 0f
+                        }
+                    }
+                })
+                .start()
+        }
     }
 }
