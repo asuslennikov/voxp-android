3600e814a6d2e485d9b998f4bd436cc953ba5cb8
Anton Suslennikov <Anton.Suslennikov@bsc-ideas.com>
2019-07-02 17:19:28 +0300

    Support for new availability watcher

--- 04_voxp_app/src/main/java/ru/voxp/android/data/network/NetworkManagerImpl.kt
+++ 04_voxp_app/src/main/java/ru/voxp/android/data/network/NetworkManagerImpl.kt
@@ -1,10 +1,16 @@
 package ru.voxp.android.data.network
 
+import android.annotation.TargetApi
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.ConnectivityManager
+import android.net.ConnectivityManager.NetworkCallback
+import android.net.Network
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import android.net.NetworkRequest
+import android.os.Build
 import io.reactivex.Observable
 import io.reactivex.subjects.BehaviorSubject
 import ru.voxp.android.data.di.ManagerScope
@@ -22,7 +28,7 @@ class NetworkManagerImpl @Inject constructor(
     init {
         availabilityPublisher = BehaviorSubject.createDefault(determineAvailabilityStatus())
         availabilityObservable = availabilityPublisher.distinctUntilChanged()
-        registerConnectionAvailabilityReceiver()
+        registerConnectionAvailabilityWatcher()
     }
 
     private fun determineAvailabilityStatus(): Boolean {
@@ -31,13 +37,38 @@ class NetworkManagerImpl @Inject constructor(
         return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
 
-    private fun registerConnectionAvailabilityReceiver() {
+    private fun registerConnectionAvailabilityWatcher() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            newConnectionAvailabilityWatcher()
+        } else {
+            oldConnectionAvailabilityWatcher()
+        }
+    }
+
+    private fun oldConnectionAvailabilityWatcher() {
         val receiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context?, intent: Intent?) {
                 availabilityPublisher.onNext(determineAvailabilityStatus())
             }
         }
         context.registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun newConnectionAvailabilityWatcher() {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkRequestBuilder = NetworkRequest.Builder()
+            .addCapability(NET_CAPABILITY_INTERNET)
+        val networkRequest = networkRequestBuilder.build()
+        connectivityManager.registerNetworkCallback(networkRequest, object : NetworkCallback() {
+            override fun onAvailable(network: Network?) {
+                availabilityPublisher.onNext(determineAvailabilityStatus())
+            }
+
+            override fun onLost(network: Network?) {
+                availabilityPublisher.onNext(determineAvailabilityStatus())
+            }
+        })
     }
 
     override fun isConnectionAvailable(): Boolean {
