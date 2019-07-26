49cefe350646733c7b114e07c24f424283d2878d
Anton Suslennikov <Anton.Suslennikov@bsc-ideas.com>
2019-07-26 23:30:56 +0300

    Simplest version of multi-viewModel

--- 04_voxp_app/src/main/java/ru/voxp/android/presentation/law/card/LawCardViewModel.kt
+++ 04_voxp_app/src/main/java/ru/voxp/android/presentation/law/card/LawCardViewModel.kt
@@ -1,11 +1,8 @@
 package ru.voxp.android.presentation.law.card
 
-import ru.jewelline.mvvm.base.presentation.AbstractViewModel
+import ru.voxp.android.presentation.core.recycler.RecyclerViewModel
 import javax.inject.Inject
 
-class LawCardViewModel @Inject constructor() : AbstractViewModel<LawCardState>() {
-    override fun buildInitialState(): LawCardState =
-        throw IllegalStateException("This view model usually should newer create new state by itself")
-
+class LawCardViewModel @Inject constructor() : RecyclerViewModel<LawCardState>() {
 
 }
